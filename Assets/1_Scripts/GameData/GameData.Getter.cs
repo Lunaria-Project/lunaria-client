083df9557e9dd408af55eb97665c65e6d63adae1
tsomsomm <tsomsomm@gmail.com>
2025-11-30 02:25:11 +0900

    마이홈에서 npc에 말걸면 컷신 재생하도록 작업

--- Assets/1_Scripts/GameData/GameData.Getter.cs
+++ Assets/1_Scripts/GameData/GameData.Getter.cs
@@ -15,4 +15,18 @@ public partial class GameData
         result.Sort((left, right) => left.Order.CompareTo(right.Order));
         return result;
     }
+
+    public List<MapNpcMenuData> GetActivatedMapNpcMenuDataListByNpcId(int npcDataId)
+    {
+        var result = new List<MapNpcMenuData>();
+        foreach (var data in _dtMapNpcMenuData)
+        {
+            if (data.NpcId != npcDataId) continue;
+            //TODO(지선): Requirement.IsSatisfied 구현하기
+            result.Add(data);
+        }
+
+        result.Sort((left, right) => left.Order.CompareTo(right.Order));
+        return result;
+    }
 }
