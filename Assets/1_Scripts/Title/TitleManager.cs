766f189cf18007c0f801ed6696e781038d2bb6bc
tsomsomm <tsomsomm@gmail.com>
2025-11-30 01:59:17 +0900

    타이틀씬에서 마이홈 씬으로 이동할 수 있도록 작업
    title

--- Assets/1_Scripts/Title/TitleManager.cs
+++ Assets/1_Scripts/Title/TitleManager.cs
@@ -1,7 +1,18 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
 public class TitleManager : SingletonMonoBehaviour<TitleManager>
 {
+    [SerializeField] private GameObject _dontDestroyOnLoadObject;
+
     public void Start()
     {
         GameData.Instance.LoadGameData();
+        DontDestroyOnLoad(_dontDestroyOnLoadObject);
+    }
+
+    public void OnGoMyhomeButtonClick()
+    {
+        SceneManager.LoadScene(1);
     }
 }
