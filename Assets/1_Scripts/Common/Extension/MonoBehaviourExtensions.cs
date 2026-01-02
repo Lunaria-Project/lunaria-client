65947d7b2ad19d858a909d01189504d7fb123240
tsomsomm <tsomsomm@gmail.com>
2026-01-02 17:35:18 +0900

    MonoBehaviourExtensions 내용 추가

---
+++ Assets/1_Scripts/Common/Extension/MonoBehaviourExtensions.cs
@@ -0,0 +1,28 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class MonoBehaviourExtensions
+{
+    public static void SetActive<T>(this T component, bool isActive) where T : MonoBehaviour
+    {
+        if (component == null) return;
+
+        component.gameObject.SetActive(isActive);
+    }
+
+    public static void SetActiveAll<T>(this IEnumerable<T> @this, bool active) where T : MonoBehaviour
+    {
+        foreach (var item in @this)
+        {
+            item.gameObject.SetActive(active);
+        }
+    }
+
+    public static void SetActiveAll(this IEnumerable<GameObject> @this, bool active)
+    {
+        foreach (var item in @this)
+        {
+            item.gameObject.SetActive(active);
+        }
+    }
+}
