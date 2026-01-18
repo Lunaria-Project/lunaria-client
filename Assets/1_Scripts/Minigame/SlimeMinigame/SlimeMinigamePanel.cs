bbe0cdd24cab34f0f513cb863ce5a8fd3edb4697
tsomsomm <tsomsomm@gmail.com>
2026-01-18 12:05:29 +0900

    [SlimeMinigame] 슬라임 패널 추가

---
+++ Assets/1_Scripts/Minigame/SlimeMinigame/SlimeMinigamePanel.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class SlimeMinigamePanel : Panel<SlimeMinigamePanel>
+{
+    protected override void OnShow(params object[] args)
+    {
+    }
+    protected override void OnHide()
+    {
+    }
+    protected override void OnRefresh()
+    {
+    }
+}
