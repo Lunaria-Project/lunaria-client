a40a3f6832c06dcdf47c17fee5fa455c6c403a86
tsomsomm <tsomsomm@gmail.com>
2026-01-19 23:31:57 +0900

    [SlimeMinigame] 미니게임 작업

---
+++ Assets/1_Scripts/Minigame/SlimeMinigame/SlimeBlock.cs
@@ -0,0 +1,55 @@
+using System;
+using Cysharp.Threading.Tasks;
+using DG.Tweening;
+using Lunaria;
+using UnityEngine;
+
+public class SlimeBlock : MonoBehaviour
+{
+    [SerializeField] private RectTransform _rectTransform;
+    [SerializeField] private GameObject _touchButton;
+    [SerializeField] private Image _slimeImage;
+    [SerializeField] private Vector2 _showPosition = Vector2.up;
+
+    public bool IsShowing { get; private set; }
+
+    private Action<int> _onTouchSlime;
+    private int _slimeOrder;
+
+    public void SetOnTouchSlime(Action<int> onTouchSlime)
+    {
+        _onTouchSlime = onTouchSlime;
+    }
+
+    public async UniTask Show(int slimeOrder, float showTime)
+    {
+        _slimeOrder = slimeOrder;
+        IsShowing = true;
+        _rectTransform.anchoredPosition = Vector2.zero;
+        _rectTransform.gameObject.SetActive(true);
+        _touchButton.SetActive(true);
+
+        DOTween.Kill(this);
+        await DOTween.Sequence().SetId(this)
+               .Append(_rectTransform.DOAnchorPos(_showPosition, 0.3f).SetEase(Ease.OutQuad))
+               .SetDelay(showTime)
+               .Append(_rectTransform.DOAnchorPos(Vector2.zero, 0.2f)).SetEase(Ease.OutQuad)
+               .AppendOnComplete(Hide)
+               .GetTask();
+    }
+
+    public void Hide()
+    {
+        IsShowing = false;
+        DOTween.Kill(this);
+        _rectTransform.anchoredPosition = Vector2.zero;
+        _rectTransform.gameObject.SetActive(false);
+        _touchButton.SetActive(false);
+    }
+
+    public void OnTouchButtonClick()
+    {
+        _onTouchSlime.Invoke(_slimeOrder);
+        Hide();
+    }
+}
--- Assets/1_Scripts/Minigame/SlimeMinigame/SlimeMinigamePanel.cs
+++ Assets/1_Scripts/Minigame/SlimeMinigame/SlimeMinigamePanel.cs
@@ -1,14 +1,99 @@
+using System.Collections;
+using System.Collections.Generic;
+using Cysharp.Threading.Tasks;
+using Lunaria;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class SlimeMinigamePanel : Panel<SlimeMinigamePanel>
 {
+    [SerializeField] private Text _remainTimeText;
+    [SerializeField] private Text _countText;
+    [SerializeField] private SlimeBlock[] _slimeBlocks;
+    [SerializeField] private int _slimeShowCount = 3;
+    [SerializeField] private float _showDelayMinSeconds = 2;
+    [SerializeField] private float _showDelayMaxSeconds = 7;
+    [SerializeField] private int _minigameSeconds = 60;
+
+    private float _remainTime;
+    private readonly List<Coroutine> _coroutines = new();
+    private int _slimeCount;
+
+    private void Awake()
+    {
+        foreach (var slimeBlock in _slimeBlocks)
+        {
+            slimeBlock.SetOnTouchSlime(OnTouchSlime);
+        }
+    }
+
+    private void Update()
+    {
+        _remainTime -= Time.deltaTime;
+        _remainTimeText.SetText(Mathf.RoundToInt(_remainTime).ToString());
+    }
+
     protected override void OnShow(params object[] args)
     {
+        _remainTime = _minigameSeconds;
+        _slimeCount = 0;
+        _countText.SetText(_slimeCount.ToPrice());
+        HideAll();
+        StartSlimeCoroutine();
+        Invoke(nameof(StopSlimeCoroutine), _minigameSeconds);
     }
+
     protected override void OnHide()
     {
+        HideAll();
     }
-    protected override void OnRefresh()
+
+    protected override void OnRefresh() { }
+
+    private void StartSlimeCoroutine()
     {
+        _coroutines.Clear();
+        for (var i = 0; i < _slimeShowCount; i++)
+        {
+            _coroutines.Add(StartCoroutine(CoShowNpcDialog()));
+        }
+    }
+
+    private void StopSlimeCoroutine()
+    {
+        foreach (var coroutine in _coroutines)
+        {
+            StopCoroutine(coroutine);
+        }
+        _coroutines.Clear();
+        HidePanel();
+    }
+
+    private IEnumerator CoShowNpcDialog()
+    {
+        while (true)
+        {
+            var randomDialogIndex = Random.Range(0, _slimeBlocks.Length);
+            var slimeBlock = _slimeBlocks.GetAt(randomDialogIndex);
+            if (slimeBlock.IsShowing) continue;
+
+            var delaySeconds = Random.Range(_showDelayMinSeconds, _showDelayMaxSeconds);
+            yield return slimeBlock.Show(1, delaySeconds).ToCoroutine(); // TODO(지선): order값 넣어야함
+            yield return new WaitForSeconds(delaySeconds);
+        }
+    }
+
+    private void HideAll()
+    {
+        foreach (var slime in _slimeBlocks)
+        {
+            slime.Hide();
+        }
+    }
+
+    private void OnTouchSlime(int slimeOrder)
+    {
+        _slimeCount++;
+        _countText.SetText(_slimeCount.ToPrice());
     }
 }
