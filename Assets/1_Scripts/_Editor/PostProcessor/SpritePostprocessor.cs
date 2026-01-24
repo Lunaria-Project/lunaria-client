93d6870c4c6b78695b8b9c48c5b8ba611ea56407
tsomsomm <tsomsomm@gmail.com>
2026-01-24 14:40:02 +0900

    이미지에 ui_ 넣으면 pixels per unit 100이 되도록 작업

--- Assets/1_Scripts/_Editor/PostProcessor/SpritePostprocessor.cs
+++ Assets/1_Scripts/_Editor/PostProcessor/SpritePostprocessor.cs
@@ -12,9 +12,14 @@ public class SpritePostprocessor : AssetPostprocessor
         var importer = (TextureImporter)assetImporter;
         if (importer.userData.Contains(AppliedFlag)) return;
 
+        var isUi = assetPath.Contains("ui_");
+
         importer.textureType = TextureImporterType.Sprite;
         importer.spriteImportMode = SpriteImportMode.Single;
-        importer.spritePixelsPerUnit = DefaultPixelsPerUnit;
+        if (!isUi)
+        {
+            importer.spritePixelsPerUnit = DefaultPixelsPerUnit;
+        }
         importer.userData = AppliedFlag;
     }
 }
