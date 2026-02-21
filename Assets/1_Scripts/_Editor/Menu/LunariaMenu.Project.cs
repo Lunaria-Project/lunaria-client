033845be4bfaf67aca55ea12b5fa2cb69b454b5d
tsomsomm <tsomsomm@gmail.com>
2026-02-22 02:21:41 +0900

    프리팹 이름도 대소문자 일괄 변경하도록 수정

--- Assets/1_Scripts/_Editor/Menu/LunariaMenu.Project.cs
+++ Assets/1_Scripts/_Editor/Menu/LunariaMenu.Project.cs
@@ -5,45 +5,45 @@ using UnityEditor;
 public static partial class LunariaMenu
 {
     private const string ChangedSuffix = "_changed";
+    private static readonly string[] TargetExtensions = { ".png", ".prefab" };
 
     [MenuItem("Lunaria/Project/Rename AssetBundle Files To LowerCase - Step1")]
     private static void RenameToLowerCaseStep1()
     {
-        var assetBundlesPath = AssetBundleManager.AssetBundlePath;
+        const string assetBundlesPath = AssetBundleManager.AssetBundlePath;
         if (!Directory.Exists(assetBundlesPath))
         {
             LogManager.LogError($"Directory not found: {assetBundlesPath}");
             return;
         }
 
-        var files = Directory.GetFiles(assetBundlesPath, "*.png", SearchOption.AllDirectories);
         var renamedCount = 0;
-
-        foreach (var filePath in files)
+        foreach (var ext in TargetExtensions)
         {
-            var directory = Path.GetDirectoryName(filePath);
-            if (directory == null) continue;
-            var fileName = Path.GetFileName(filePath);
-            var lowerFileName = fileName.ToLower();
-            if (string.Equals(fileName, lowerFileName)) continue;
-
-            // icon.png -> icon_changed.png
-            var ext = Path.GetExtension(lowerFileName);
-            var nameWithoutExt = Path.GetFileNameWithoutExtension(lowerFileName);
-            var newFileName = nameWithoutExt + ChangedSuffix + ext;
-            var newPath = Path.Combine(directory, newFileName);
-
-            File.Move(filePath, newPath);
-            renamedCount++;
-            LogManager.Log($"Step1: {fileName} -> {newFileName}");
-
-            // icon.png.meta -> icon_changed.png.meta
-            var metaPath = filePath + ".meta";
-            if (File.Exists(metaPath))
+            var files = Directory.GetFiles(assetBundlesPath, "*" + ext, SearchOption.AllDirectories);
+            foreach (var filePath in files)
             {
-                var newMetaPath = newPath + ".meta";
-                File.Move(metaPath, newMetaPath);
-                LogManager.Log($"Step1: {fileName}.meta -> {newFileName}.meta");
+                var directory = Path.GetDirectoryName(filePath);
+                if (directory == null) continue;
+                var fileName = Path.GetFileName(filePath);
+                var lowerFileName = fileName.ToLower();
+                if (string.Equals(fileName, lowerFileName)) continue;
+
+                var nameWithoutExt = Path.GetFileNameWithoutExtension(lowerFileName);
+                var newFileName = nameWithoutExt + ChangedSuffix + ext;
+                var newPath = Path.Combine(directory, newFileName);
+
+                File.Move(filePath, newPath);
+                renamedCount++;
+                LogManager.Log($"Step1: {fileName} -> {newFileName}");
+
+                var metaPath = filePath + ".meta";
+                if (File.Exists(metaPath))
+                {
+                    var newMetaPath = newPath + ".meta";
+                    File.Move(metaPath, newMetaPath);
+                    LogManager.Log($"Step1: {fileName}.meta -> {newFileName}.meta");
+                }
             }
         }
 
@@ -54,40 +54,39 @@ public static partial class LunariaMenu
     [MenuItem("Lunaria/Project/Rename AssetBundle Files To LowerCase - Step2")]
     private static void RenameToLowerCaseStep2()
     {
-        var assetBundlesPath = AssetBundleManager.AssetBundlePath;
+        const string assetBundlesPath = AssetBundleManager.AssetBundlePath;
         if (!Directory.Exists(assetBundlesPath))
         {
             LogManager.LogError($"Directory not found: {assetBundlesPath}");
             return;
         }
 
-        var files = Directory.GetFiles(assetBundlesPath, "*" + ChangedSuffix + ".png", SearchOption.AllDirectories);
         var renamedCount = 0;
-
-        foreach (var filePath in files)
+        foreach (var ext in TargetExtensions)
         {
-            var directory = Path.GetDirectoryName(filePath);
-            if (directory == null) continue;
-            var fileName = Path.GetFileName(filePath);
-
-            // icon_changed.png -> icon.png
-            var ext = Path.GetExtension(fileName);
-            var nameWithoutExt = Path.GetFileNameWithoutExtension(fileName);
-            var originalName = nameWithoutExt.Substring(0, nameWithoutExt.Length - ChangedSuffix.Length);
-            var newFileName = originalName + ext;
-            var newPath = Path.Combine(directory, newFileName);
-
-            File.Move(filePath, newPath);
-            renamedCount++;
-            LogManager.Log($"Step2: {fileName} -> {newFileName}");
-
-            // icon_changed.png.meta -> icon.png.meta
-            var metaPath = filePath + ".meta";
-            if (File.Exists(metaPath))
+            var files = Directory.GetFiles(assetBundlesPath, "*" + ChangedSuffix + ext, SearchOption.AllDirectories);
+            foreach (var filePath in files)
             {
-                var newMetaPath = newPath + ".meta";
-                File.Move(metaPath, newMetaPath);
-                LogManager.Log($"Step2: {fileName}.meta -> {newFileName}.meta");
+                var directory = Path.GetDirectoryName(filePath);
+                if (directory == null) continue;
+                var fileName = Path.GetFileName(filePath);
+
+                var nameWithoutExt = Path.GetFileNameWithoutExtension(fileName);
+                var originalName = nameWithoutExt.Substring(0, nameWithoutExt.Length - ChangedSuffix.Length);
+                var newFileName = originalName + ext;
+                var newPath = Path.Combine(directory, newFileName);
+
+                File.Move(filePath, newPath);
+                renamedCount++;
+                LogManager.Log($"Step2: {fileName} -> {newFileName}");
+
+                var metaPath = filePath + ".meta";
+                if (File.Exists(metaPath))
+                {
+                    var newMetaPath = newPath + ".meta";
+                    File.Move(metaPath, newMetaPath);
+                    LogManager.Log($"Step2: {fileName}.meta -> {newFileName}.meta");
+                }
             }
         }
 
