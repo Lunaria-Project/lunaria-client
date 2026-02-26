4262626adfe91d5df787ac2a21ee65fb5d1522f5
tsomsomm <tsomsomm@gmail.com>
2026-02-26 21:27:17 +0900

    [Map] hide된 npc와 상호작용되지 않게 수정

--- Assets/1_Scripts/Map/Object/NpcObject.cs
+++ Assets/1_Scripts/Map/Object/NpcObject.cs
@@ -5,8 +5,9 @@ public class NpcObject : MovableObject
     [SerializeField] private NpcCompassUI _compassUI;
 
     public NpcCompassUI CompassUI => _compassUI;
-    private int _characterDataId;
     public float DistanceToPlayer { get; private set; }
+    private int _characterDataId;
+    private bool _isShown;
 
     public void Show(Generated.MapNpcPositionData positionData)
     {
@@ -18,16 +19,19 @@ public class NpcObject : MovableObject
         var initSpriteScale = infoData.SpritePositionAndScale.GetAt(2);
         InitPositionAndScale(initPosition, initSpritePosition, initSpriteScale, infoData.ColliderScale);
         _compassUI.Init(new Vector2(infoData.CompassUIPosition.GetAt(0), infoData.CompassUIPosition.GetAt(1)), positionData.NpcId);
+        _isShown = true;
     }
 
     public void SetIsNearBy(bool isNearBy, float distance)
     {
+        if (!_isShown) return;
         _compassUI.SetIsNearBy(isNearBy);
         DistanceToPlayer = distance;
     }
     
     public void Hide()
     {
+        _isShown = false;
         gameObject.SetActive(false);
         _compassUI.OnHide();
     }
