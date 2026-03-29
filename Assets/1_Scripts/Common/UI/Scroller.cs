4505f67a00c40dd0656a011bc78038db61d9c3d3
tsomsomm <tsomsomm@gmail.com>
2026-03-29 16:01:19 +0900

    [ui] Scroller 수정

--- Assets/1_Scripts/Common/UI/Scroller.cs
+++ Assets/1_Scripts/Common/UI/Scroller.cs
@@ -1,14 +1,12 @@
 using UnityEngine;
-using UnityEngine.UI;
 
-public partial class Scroller : MonoBehaviour
+public class Scroller : MonoBehaviour
 {
-    [SerializeField] private RawImage _img;
+    [SerializeField] private Lunaria.Image _image;
     [SerializeField] private float x, y;
 
     private void Update()
     {
-     
-        _img.uvRect = new Rect(_img.uvRect.position + new Vector2(x, y) * Time.deltaTime, _img.uvRect.size);
+        _image.material.mainTextureOffset += new Vector2(x, y) * Time.deltaTime;
     }
 }
