8f34fab48de55f2d09b7f01a123719ea43f2a4e2
tsomsomm <tsomsomm@gmail.com>
2026-04-05 02:04:05 +0900

    [UI] Scroller material 이슈 수정

--- Assets/1_Scripts/Common/UI/Scroller.cs
+++ Assets/1_Scripts/Common/UI/Scroller.cs
@@ -5,8 +5,24 @@ public class Scroller : MonoBehaviour
     [SerializeField] private Lunaria.Image _image;
     [SerializeField] private float x, y;
 
+    private Material _materialInstance;
+
+    private void Awake()
+    {
+        _materialInstance = new Material(_image.material);
+        _image.material = _materialInstance;
+    }
+
     private void Update()
     {
-        _image.material.mainTextureOffset += new Vector2(x, y) * Time.deltaTime;
+        _materialInstance.mainTextureOffset += new Vector2(x, y) * Time.deltaTime;
+    }
+
+    private void OnDestroy()
+    {
+        if (_materialInstance != null)
+        {
+            Destroy(_materialInstance);
+        }
     }
 }
