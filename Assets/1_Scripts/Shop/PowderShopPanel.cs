737cf3372e4a864062c39c0b989e87f4ae948eb7
tsomsomm <tsomsomm@gmail.com>
2026-04-09 20:32:44 +0900

    코드 정리

--- Assets/1_Scripts/Shop/PowderShopPanel.cs
+++
@@ -1,9 +0,0 @@
-public class PowderShopPanel : Panel<PowderShopPanel>
-{
-    protected override void OnShow(params object[] args)
-    {
-    }
-    protected override void OnHide()
-    {
-    }
-}
