a27535c75573326cb859d5cb62ef27d6a8dabd29
tsomsomm <tsomsomm@gmail.com>
2026-08-25 13:04:19 +0900

    ImageExtension 함수 추가

--- Assets/1_Scripts/Common/Extension/ImageExtension.cs
+++ Assets/1_Scripts/Common/Extension/ImageExtension.cs
@@ -1,10 +1,18 @@
 using UnityEngine;
-using UnityEngine.UI;
+using Image = Lunaria.Image;
 
 public static class ImageExtension
 {
     public static void SetSprite(this Image image, Sprite sprite)
     {
         image.sprite = sprite;
+    }
+
+    public static void SetSprites(this Image[] images, Sprite sprite)
+    {
+        foreach (var image in images)
+        {
+            image.SetSprite(sprite);
+        }
     }
 }
